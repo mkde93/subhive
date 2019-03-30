cde7c11e4c284674b302addde68145f336bb2485
Francisco Souza <Franciscosouza92@gmail.com>
2019-03-30 02:22:33 +0100

    Added all Events + Artists for the website currently + Posters for the first 5 Subhive events.

--- subhive/src/data/Events.tsx
+++ subhive/src/data/Events.tsx
@@ -1,9 +1,98 @@
 export default {
-    halloween1: {
+    subhive_1: {
+        title: "Subhive",
+        date: "22.8.2017",
+        location: "Platform 4, Aalborg",
+        eventlink: "https://www.facebook.com/events/332780630494012/",
+        aftermovie: "false",
+        bgimg: "Subhive_5_bg.png",
+        poster: "Subhive_1_Poster.png",
+        desc: "SUBHIVE is a blend between underground bass music, mind " +
+            "melting visuals and flickering lights. Focusing on future bass, " +
+            "trap and drum & bass, SUBHIVE is set to shake up the underground " +
+            "music scene of Aalborg.",
+        lineup: ["Shogunglow", "Czech Strings", "Meluran", "Kasger", "Lazykid" + "Tazer", "Vidya Vidya"],
+        settimes: ["21.00-22.00", "22.00-23.00", "23.00-00.00", "00.00-01.00", "01.00-02.00", "02.00-03.00"],
+    },
+    subhive_2: {
+        title: "Subhive - Winter Edition",
+        date: "24.11.2017",
+        location: "Platform 4, Aalborg",
+        eventlink: "https://www.facebook.com/events/141204753293200/",
+        aftermovie: "false",
+        bgimg: "Subhive_5_bg.png",
+        poster: "Subhive_2_Poster.png",
+        desc: "SUBHIVE is a blend between underground bass music, mind " +
+            "melting visuals and flickering lights. Focusing on future bass, " +
+            "trap and drum & bass, SUBHIVE is set to shake up the underground " +
+            "music scene of Aalborg.",
+        lineup: ["Whoknows", "Cuuts", "Vidya Vidya", "Redemptive", "Kasger", "Meluran"],
+        settimes: ["21.00-22.00", "22.00-23.00", "23.00-00.00", "00.00-01.00", "01.00-02.00", "02.00-03.00"],
+    },
+    subhive_3: {
+        title: "Subhive",
+        date: "09.03.2018",
+        location: "Platform 4, Aalborg",
+        eventlink: "https://www.facebook.com/events/181674995774119/",
+        aftermovie: "false",
+        bgimg: "Subhive_5_bg.png",
+        poster: "Subhive_3_Poster.png",
+        desc: "SUBHIVE is a blend between underground bass music, mind " +
+            "melting visuals and flickering lights. Focusing on future bass, " +
+            "trap and drum & bass, SUBHIVE is set to shake up the underground " +
+            "music scene of Aalborg.",
+        lineup: ["KEVO", "Shio" + " B2B " + "Andreas Northland", "Kasger", "TAZER", "Vidya Vidya"],
+        settimes: ["22.00-23.00", "23.00-00.00", "00.00-01.00", "01.00-02.00", "02.00-03.00"],
+    },
+    subhive_4: {
+        title: "Subhive - Safari Expedition",
+        date: "28.04.2018",
+        location: "Platform 4, Aalborg",
+        eventlink: "https://www.facebook.com/events/421238378320285/",
+        aftermovie: "false",
+        bgimg: "Subhive_5_bg.png",
+        poster: "Subhive_3_Poster.png",
+        desc: "SUBHIVE is a blend between underground bass music, mind " +
+            "melting visuals and flickering lights. Focusing on future bass, " +
+            "trap and drum & bass, SUBHIVE is set to shake up the underground " +
+            "music scene of Aalborg.",
+        lineup: ["SUBHIVE Warmup Session", "Czech Strings", "Runge", "CELO", "Meluran", "O.V."],
+        settimes: ["21.00-22.00", "22.00-23.00", "23.00-00.00", "00.00-01.00", "01.00-02.00", "02.00-03.00"],
+    },
+    subvibe_1: {
+        title: "SubVibe - Outdoor Chillout Bass Music",
+        date: "08.06.2018",
+        location: "Østre Anlæg",
+        eventlink: "https://www.facebook.com/events/2099007947054258/",
+        aftermovie: "false",
+        bgimg: "Subhive_5_bg.png",
+        poster: "Subhive_3_Poster.png",
+        desc: "SUBHIVE presents SubVibe. A Few small summer gatherings " +
+            "for SUBHIVE fans. Bring your own beer and enjoy some chillout " +
+            "bass music in the sun ",
+        lineup: ["Kasger", "Meluran", "TAZER", "Haszan & Overload"],
+        settimes: ["13.00-15.00", "15.00-17.00", "17.00-19.00", "19.00-21.00"],
+    },
+    subhive_5: {
+        title: "Subhive",
+        date: "31.08.2018",
+        location: "Platform 4, Aalborg",
+        eventlink: "https://www.facebook.com/events/240958616732265/",
+        aftermovie: "false",
+        bgimg: "Subhive_5_bg.png",
+        poster: "Subhive_5_Poster.png",
+        desc: "SUBHIVE is a blend between underground bass music, mind " +
+            "melting visuals and flickering lights. Focusing on future bass, " +
+            "trap and drum & bass, SUBHIVE is set to shake up the underground " +
+            "music scene of Aalborg.",
+        lineup: ["Sundr", "Vivien Oldy", "TAZER", "Lasu", "Kasger", "Lazykid"],
+        settimes: ["21.00-22.00", "22.00-23.00", "23.00-00.00", "00.00-01.00", "01.00-02.00", "02.00-03.00"],
+    },
+    subhive_6: {
         title: "Subhive - Halloween Edition",
         date: "26.10.2018",
         location: "Platform 4, Aalborg",
-        eventlink: "https://www.facebook.com/events/240958616732265/",
+        eventlink: "https://www.facebook.com/events/185206485709939/",
         aftermovie: "false",
         bgimg: "Subhive_5_bg.png",
         poster: "Subhive_5_Poster.png",
@@ -11,22 +100,23 @@ export default {
             "melting visuals and flickering lights. Focusing on future bass, " +
             "trap and drum & bass, SUBHIVE is set to shake up the underground " +
             "music scene of Aalborg.",
-        lineup: ["O.V.", "SHIO", "BOBASTIC B2B DJ 0 APE", "CAPTIVE", "MELURAN"],
-        settimes: ["22.00-23.00", "23.00-00.00", "00.00-01.00", "01.00-02.00", "02.00-03.00"],
+        lineup: ["SUBHIVE Jam Session", "O.V.", "SHIO", "BOBASTIC" + " B2B " + "DJ 0 APE", "Captive", "Meluran"],
+        settimes: ["21.00-22.00", "22.00-23.00", "23.00-00.00", "00.00-01.00", "01.00-02.00", "02.00-03.00"],
     },
-    halloween2: {
-        title: "Subhive - Halloween Edition",
-        date: "29.10.2018",
-        location: "Platform 4, Aalborg",
-        eventlink: "https://www.facebook.com/events/240958616732265/",
+    subfryd_1: {
+        title: "Subhive1000 - DnB Edition",
+        date: "19.04.2019",
+        location: "1000Fryd, Aalborg",
+        eventlink: "https://www.facebook.com/events/552362531837723/",
         aftermovie: "false",
         bgimg: "Subhive_5_bg.png",
         poster: "Subhive_5_Poster.png",
         desc: "SUBHIVE is a blend between underground bass music, mind " +
-            "melting visuals and flickering lights. Focusing on future bass, " +
-            "trap and drum & bass, SUBHIVE is set to shake up the underground " +
-            "music scene of Aalborg.",
-        lineup: ["O.V.", "SHIO", "BOBASTIC B2B DJ 0 APE", "CAPTIVE", "MELURAN"],
-        settimes: ["22.00-23.00", "23.00-00.00", "00.00-01.00", "01.00-02.00", "02.00-03.00"],
-    }
+            "melting visuals and flickering lights. With our new line of smaller " +
+            "events on 1000Fryd, we strive to deliver a more intimate and intense " +
+            "experience than ever before! This night we will be playing Drum and Bass " +
+            "all night long",
+        lineup: ["Kasger", "Meluran", "TAZER"],
+        settimes: ["21.00-23.00", "23.00-01.00", "01.00-03.00"],
+    },
 }
